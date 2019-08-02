530ea401682bf55e81247e629a8af0d0b1090764
Prasanna <vijayasree.prasanna@siemens-healthineers.com>
2019-08-02 11:38:04 +0530

    SBC team

---
+++ Greeting.cs
@@ -0,0 +1,7 @@
+class Greeting
+{
+public string Greeting()
+{
+return "Namaskara";
+}
+}
--- Superman.cs
+++ Superman.cs
@@ -4,4 +4,9 @@ public int Sum(int i,intj)
 {
 return i+j;
 }
+public int Product(int i,int j)
+{
+	return i*j;
+}
+
 }
